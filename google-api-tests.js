ad3d6f1405ebb2a58d2fa6d5d21cdfc914f71ea4
Tom Coleman <tom@thesnail.org>
2014-07-09 16:03:02 +1000

    Got tests working with promises lib.

--- google-api-tests.js
+++ google-api-tests.js
@@ -27,9 +27,11 @@ var callAync = function(method, url, params, callback) {
   Meteor.setTimeout(function() {
     if (params.headers && params.headers.Authorization && 
         params.headers.Authorization.match(/bad/)) {
+      console.log('returning 401')
       return callback({response: {statusCode: 401}});
     }
 
+    console.log('returning', self.nextResult, 'from Http.call')
     return callback(null, {
       statusCode: 200,
       data: self.nextResult
@@ -39,33 +41,78 @@ var callAync = function(method, url, params, callback) {
 
 
 if (Meteor.isServer) {
-  Tinytest.add('GoogleApi - Server - get basic', function(test) {
-    var userId =  Meteor.users.insert({
-      _id: 'mockedUserId' + test.id,
-      services: {google: {
-        accessToken: 'good',
-        refreshToken: '456'
-      }}
-    });
-
+  Meteor.publish('user', function() {
+    return Meteor.users.find(this.userId);
+  });
+  
+  Meteor.methods({
+    mockUser: function(testId, token) {
+      var id = Meteor.users.insert({
+        _id: 'mockedUserId' + testId,
+        services: {google: {
+          accessToken: token,
+          refreshToken: '456'
+        }}
+      });
+      
+      return id
+    },
+    
+    loginAs: function(userId) {
+      this.setUserId(userId);
+      return {id: userId};
+    }
+  })
+  
+  Tinytest.add('GoogleApi - Sync - get basic', function(test) {
+    var userId = Meteor.call('mockUser', test.id, 'good');
+    
     HTTP.nextResult = 'foo';
     var result = GoogleApi.get('/foo/bar', {user: Meteor.users.findOne(userId)});
 
     test.equal(result, 'foo');
   });
   
-  Tinytest.add('GoogleApi - Server - get with refresh', function(test) {
-    var userId =  Meteor.users.insert({
-      _id: 'mockedUserId' + test.id,
-      services: {google: {
-        accessToken: 'bad',
-        refreshToken: '456'
-      }}
-    });
+  Tinytest.add('GoogleApi - Sync - get with refresh', function(test) {
+    var userId = Meteor.call('mockUser', test.id, 'bad');
 
     HTTP.nextResult = 'foo';
     var result = GoogleApi.get('/foo/bar', {user: Meteor.users.findOne(userId)});
 
     test.equal(result, 'foo');
   });
+} else {
+  var loginAs = function(userId, cb) {
+    Accounts.callLoginMethod({
+      methodName: 'loginAs',
+      methodArguments: [userId],
+      userCallback: function() { Meteor.subscribe('user', cb) }});
+  }
+  
+  // same tests with promises
+  
+  Tinytest.addAsync('GoogleApi - Promises - get basic', function(test, done) {
+    Meteor.call('mockUser', test.id, 'good', function(error, userId) {
+      loginAs(userId, function() {
+        HTTP.nextResult = 'foo';
+        GoogleApi.get('/foo/bar', {}).then(function(result) {
+          test.equal(result, 'foo');
+          done();
+        });
+      });
+    });
+  });
+  
+  Tinytest.addAsync('GoogleApi - Promises - get with refresh', function(test, done) {
+    Meteor.call('mockUser', test.id, 'bad', function(error, userId) {
+      loginAs(userId, function() {
+        HTTP.nextResult = 'foo';
+        GoogleApi.get('/foo/bar', {}).then(function(result) {
+          test.equal(result, 'foo');
+          done();
+        });
+      });
+    });
+  });
+  
 }
