5c3518e8e834e38e3d8d21dbe73b6131ab87085b
Jordan Brant Baker <jbb@scryent.com>
2015-08-29 00:59:18 -0700

    create a copy of options before modifying to prevent problems in callbacks

--- google-api-async.js
+++ google-api-async.js
@@ -8,9 +8,9 @@ GoogleApi = {
   _callAndRefresh: function(method, path, options, callback) {
     var self = this;
     options = options || {};
-        
-    self._call(method, path, options,       
-      // need to bind the env here so we can do mongo writes in the callback 
+
+    self._call(method, path, options,
+      // need to bind the env here so we can do mongo writes in the callback
       // (when refreshing), if we call this on the server
       Meteor.bindEnvironment(function(error, result) {
         if (error && error.response && error.response.statusCode == 401) {
@@ -19,12 +19,12 @@ GoogleApi = {
           return self._refresh(options.user, function(error) {
             if (error)
               return callback(error);
-            
+
             // if we have the user, we'll need to re-fetch them, as their
             // access token will have changed.
             if (options.user)
               options.user = Meteor.users.findOne(options.user._id);
-            
+
             self._call(method, path, options, callback);
           });
         } else {
@@ -32,12 +32,13 @@ GoogleApi = {
         }
     }, 'Google Api callAndRefresh'));
   },
-  
+
   // call a GAPI Meteor.http function if the accessToken is good
   _call: function(method, path, options, callback) {
     Log('GoogleApi._call, path:' + path);
-    
-    options = options || {};
+
+    // copy existing options to modify
+    options = _.extend({}, options)
     var user = options.user || Meteor.user();
     delete options.user;
 
@@ -45,7 +46,7 @@ GoogleApi = {
         user.services.google.accessToken) {
       options.headers = options.headers || {};
       options.headers.Authorization = 'Bearer ' + user.services.google.accessToken;
-    
+
       HTTP.call(method, this._host + '/' + path, options, function(error, result) {
         callback(error, result && result.data);
       });
