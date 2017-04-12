d8aff1463b8e4f85d0a6352a131e3d8a56a6285e
ro6 <ro6@users.noreply.github.com>
2017-04-12 16:57:18 -0700

    needed to add version constraint on 'google-oauth' for Atmosphere publish #40

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'percolate:google-api',
   summary: "A Meteor library to interact with Google's API",
-  version: '1.0.4',
+  version: '1.0.5',
   git: 'https://github.com/percolatestudio/meteor-google-api'
 });
 
 Package.on_use(function (api, where) {
   if (api.versionsFrom) {
     api.versionsFrom('0.9.0');
-    api.use(['http', 'livedata', 'google-oauth', 'mrt:q@1.0.1', 'accounts-base', 'underscore']);
+    api.use(['http', 'livedata', 'google-oauth@1.2.0', 'mrt:q@1.0.1', 'accounts-base', 'underscore']);
   } else {
-    api.use(['http', 'livedata', 'google-oauth', 'q', 'accounts-base', 'underscore']);
+    api.use(['http', 'livedata', 'google-oauth@1.2.0', 'q', 'accounts-base', 'underscore']);
   }
 
   api.add_files(['utils.js', 'google-api-async.js'], ['client', 'server']);
