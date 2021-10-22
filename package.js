3f0b683c55ce02e9383150d7586c80e9f586f591
Jan Dvorak <storytellercz@protonmail.com>
2021-10-22 20:10:30 +0200

    Update package.js for modern compatibility

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
   name: 'percolate:google-api',
   summary: "A Meteor library to interact with Google's API",
-  version: '1.0.5',
+  version: '1.1.0',
   git: 'https://github.com/percolatestudio/meteor-google-api'
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
   if (api.versionsFrom) {
-    api.versionsFrom('0.9.0');
-    api.use(['http', 'livedata', 'google-oauth@1.2.0', 'mrt:q@1.0.1', 'accounts-base', 'underscore']);
+    api.versionsFrom(['1.0', '2.3']);
+    api.use(['http', 'livedata', 'google-oauth', 'mrt:q@1.0.1', 'accounts-base', 'underscore']);
   } else {
     api.use(['http', 'livedata', 'google-oauth@1.2.0', 'q', 'accounts-base', 'underscore']);
   }
 
-  api.add_files(['utils.js', 'google-api-async.js'], ['client', 'server']);
-  api.add_files(['google-api-methods.js'], ['server']);
+  api.addFiles(['utils.js', 'google-api-async.js'], ['client', 'server']);
+  api.addFiles(['google-api-methods.js'], ['server']);
 
   api.export('GoogleApi', ['client', 'server']);
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use([
     'percolate:google-api',
     'tinytest',
